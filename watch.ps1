4472d2f9b927447b35e80a026f8fc984c592214b
Andre Attamimi <73837775+sionta@users.noreply.github.com>
2025-03-04 00:42:20 +0900

    cleaner

---
+++ watch.ps1
@@ -0,0 +1,11 @@
+$JEKYLL_CONFIG = '_config.yml,_config.dev.yml'
+$JEKYLL_SOURCE = './src'
+
+bundle exec jekyll clean `
+    --source "$JEKYLL_SOURCE" `
+    --config "$JEKYLL_CONFIG"
+
+bundle exec jekyll serve `
+    --source "$JEKYLL_SOURCE" `
+    --config "$JEKYLL_CONFIG" `
+    --watch
